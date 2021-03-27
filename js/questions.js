25054658b962458e11a7ebc74393c24979f0a11e
muhrafitriandi <61121146+muhrafitriandi@users.noreply.github.com>
2021-03-27 20:18:46 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "COVID-19 adalah singkatan dari?",
+        answer: "Coronavirus Disease - 2019",
+        options: [
+            "Coronavirus Indonesia - 2019",
+            "Corona Desinfektan - 2019",
+            "Coronavirus Disease - 2019",
+            "Corona International Disease - 2019"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Virus corona menyebar melalui?",
+        answer: "Batuk, bersin, dan air liur",
+        options: [
+            "Batuk, bersin, dan perantara nyamuk",
+            "Batuk, bersin, dan air liur",
+            "Batuk, air liur, dan perantara nyamuk",
+            "Bersin, air liur, dan perantara nyamuk"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Virus corona dapat menempel di benda-benda sekitar yang kita pegang dapat masuk melalui bagian tubuh kita, kecuali?",
+        answer: "Telinga",
+        options: [
+            "Hidung",
+            "Mata",
+            "Mulut",
+            "Telinga"
+        ]
+    },
+    
+    {
+        numb: 4,
+        question: "Langkah-langkah berikut yang tidak dapat menolongmu untuk terhindar dari virus corona ketika kamu sehat adalah?",
+        answer: "Jangan memakai cairan pembersih tangan yang mengandung alkohol",
+        options: [
+            "Jangan terlalu sering memegang wajah",
+            "Sering cuci tangan pakai sabun",
+            "Jangan memakai cairan pembersih tangan yang mengandung alkohol",
+            "Menjaga jarak dari orang yang batuk atau bersin"
+        ]
+    },
+    
+    {
+        numb: 5,
+        question: "Jarak yang aman untuk kamu berada di sekitar orang yang batuk atau bersin di sekitarmu adalah?",
+        answer: "1 meter",
+        options: [
+            "1 meter",
+            "0,5 meter",
+            "30 centimeter",
+            "80 centimeter"
+        ]
+    },    
+];
